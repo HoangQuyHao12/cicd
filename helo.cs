a3f5c90e610253d2b93e0dd09df1001ef55d7b60
hoangquyhao <77380197+hoangquyhao@users.noreply.github.com>
2022-11-26 00:35:12 +0700

    hao

---
+++ helo.cs
@@ -0,0 +1,13 @@
+//viet chuong trinh co ban cho cs
+//viet chuong trinh co ban cho c#
+using System;
+namespace helo
+{
+    internal class programming
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
